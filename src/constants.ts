69200c3d212b938671991ce6855d1d2013bc098a
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-06-18 13:12:45 +0100

    add qwen model

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,10 @@
 export const modelsInfo = {
+	"qwen/qwen2.5-vl-32b-instruct": 	{
+			name: "Qwen 2.5 vl",
+			requireApiKey: false,
+			supportVision: true,
+
+		},
 	"google/gemini-2.5-flash-preview-05-20": {
 		name: "Gemini 2.5 Flash Preview",
 		requireApiKey: false,
@@ -18,9 +24,10 @@ export const modelsInfo = {
 
 // duplicated to please zod
 export const SUPPORTED_MODELS = [
+	"qwen/qwen2.5-vl-32b-instruct", 
 	"google/gemini-2.5-flash-preview-05-20",
-	"anthropic/claude-sonnet-4",
 	"openai/gpt-4o-mini",
+	"anthropic/claude-sonnet-4",
 ] as const;
 
 export type AvailableModels = (typeof SUPPORTED_MODELS)[number];
