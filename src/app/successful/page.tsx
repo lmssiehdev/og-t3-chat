ee0f2bdc7bca7c4f5bc1e77376d5cf21589ef991
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-06-19 20:49:17 +0100

    update .env.example

---
+++ src/app/successful/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { useEffect } from "react";
+
+export default async function LoginPage() {
+
+    useEffect(() => {
+        window.location.href = "/chat"
+    }, [])
+	return (
+		<div className="flex min-h-screen items-center justify-center p-8">
+			<div className="space-y-2">
+				<span className=" block text-center w-fit text-neutral-400">
+                    Login successful, redirecting to chat...
+				</span>
+			</div>
+		</div>
+	);
+}
